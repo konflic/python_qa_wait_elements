f3e082ecfad4138f9d8387af18b3629626032719
konflic <konflic@mail.ru>
2026-01-27 19:55:28 +0300

    Версия 4.40

--- 1_selenium_waits/test_implicit_wait.py
+++
@@ -1,7 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-def test_check_header(browser):
-    browser.implicitly_wait(5)
-    browser.get("https://konflic.github.io/examples/pages/slowlyloading.html")
-    browser.find_element(By.CSS_SELECTOR, "#header")
